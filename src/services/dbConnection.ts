14dff21c85fd5182346148ebaed1993867754c34
maxime <dahaka18@gmail.com>
2022-06-04 14:50:56 +0200

    prepare for prod test

--- src/services/dbConnection.ts
+++ src/services/dbConnection.ts
@@ -1,3 +1,5 @@
 const {Sequelize} = require('sequelize');
-//export const sequelize = new Sequelize('mysql://sql11495465:58R7lyRMvS@sql11.freemysqlhosting.net:3306/sql11495465');
-export const sequelize = new Sequelize('mysql://root:root@localhost:8889/twitter');
+const dotenv = require('dotenv');
+dotenv.config();
+
+export const sequelize = new Sequelize(process.env.DB_CONNECTION);
